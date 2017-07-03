3a76d38d953baf84aa9f00f289c13d156f8ecdcd
Dimitar <dimitar.lukarski@gmail.com>
2017-07-03 15:47:46 +0300

    Improved PlayerForm code

---
+++ MediaPlayer/PlayerForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MediaPlayer
+{
+    partial class PlayerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayerForm));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lvItems = new System.Windows.Forms.ListView();
+            this.title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Location = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(239, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(839, 566);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(221, 53);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Files\r\n";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lvItems
+            // 
+            this.lvItems.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.lvItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.title,
+            this.Location});
+            this.lvItems.Location = new System.Drawing.Point(12, 71);
+            this.lvItems.Name = "lvItems";
+            this.lvItems.RightToLeftLayout = true;
+            this.lvItems.Size = new System.Drawing.Size(221, 460);
+            this.lvItems.TabIndex = 2;
+            this.lvItems.UseCompatibleStateImageBehavior = false;
+            this.lvItems.View = System.Windows.Forms.View.Details;
+            this.lvItems.DoubleClick += new System.EventHandler(this.lvItems_DoubleClick);
+            // 
+            // title
+            // 
+            this.title.Text = "Title";
+            this.title.Width = 104;
+            // 
+            // Location
+            // 
+            this.Location.Text = "Location";
+            this.Location.Width = 419;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 537);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(107, 41);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Play all";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_click);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(1088, 585);
+            this.shapeContainer1.TabIndex = 4;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 556;
+            this.lineShape1.X2 = 658;
+            this.lineShape1.Y1 = 203;
+            this.lineShape1.Y2 = 316;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(125, 537);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(108, 41);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // PlayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1088, 585);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lvItems);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PlayerForm";
+            this.Text = "Media Player";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListView lvItems;
+        private System.Windows.Forms.ColumnHeader title;
+        private System.Windows.Forms.ColumnHeader Location;
+        private System.Windows.Forms.Button btnPlay;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ MediaPlayer/PlayerForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class PlayerForm : Form
+    {
+        public PlayerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFiles()
+        {
+            //open dialog and select a file
+            Stream myStream = null;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = " MP3 Audio File (*.mp3)|*.mp3|Audio Video Interleave (*.avi)|*.avi|Windows Media File (*.wma)|*.wma|WAV Audio File  (*.wav)|*.wav|All files (*.*)|*.*";
+            dialog.FilterIndex = 0;
+            dialog.RestoreDirectory = false;
+            dialog.Multiselect = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = dialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            string[] fileNameandPath = dialog.FileNames;
+                            string[] filename = dialog.SafeFileNames;
+
+                            for (int i = 0; i < dialog.SafeFileNames.Count(); i++)
+                            {
+                                string[] saveItem = new string[2];
+
+                                saveItem[0] = filename[i];
+                                saveItem[1] = fileNameandPath[i];
+
+                                ListViewItem lvi = new ListViewItem(saveItem);
+
+                                lvItems.Items.Add(lvi);
+                            }
+                        }
+                    }
+                }
+
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Error Couldn't find file on disk");
+                }
+            }
+        }
+
+        private void PlayAllItems()
+        {
+            WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myplaylist");
+            WMPLib.IWMPMedia media;
+
+            for (int i = 0; i < lvItems.Items.Count; i++)
+            {
+                int ii = 1;
+                media = axWindowsMediaPlayer1.newMedia(lvItems.Items[i].SubItems[ii].Text);
+                playlist.appendItem(media);
+                ii++;
+                axWindowsMediaPlayer1.currentPlaylist = playlist;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+
+        //double click on a song for play
+        private void lvItems_DoubleClick(object sender, EventArgs e)
+        {
+            string selectedFile = lvItems.FocusedItem.SubItems[1].Text;
+
+            axWindowsMediaPlayer1.URL = @selectedFile;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadFiles();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lvItems.Items.Clear();
+        }
+
+        private void btnPlay_click(object sender, EventArgs e)
+        {
+            PlayAllItems();
+        }
+    }
+}
